0c8d49cc4608973218e7010554b5f57f8a826541
Joel Rocha <rochab@gmail.com>
2022-01-23 12:32:22 -0300

    banner

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,7 +2,9 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta property="og:locale" content="pt_BR" />
 
         <title>@yield('title')</title>
 
