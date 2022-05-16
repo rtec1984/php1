79eb37dacec0a0babf2dbf1365510b928a8831f5
Joel Rocha <rochab@gmail.com>
2022-05-16 17:55:06 -0300

    Ranking

--- resources/views/ranking.blade.php
+++ resources/views/ranking.blade.php
@@ -8,35 +8,51 @@
     <h1>Ranking:</h1>
 
     <div class="row">
-            <div class="col-auto">
-                <br>
-                <table class="table table-bordered table-striped table-hover table-light table-responsive" id="ranking">
-    
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">NOME</th>
-                            <th scope="col">P</th>
-                            <th scope="col">J</th>
-                            <th scope="col">V</th>
-                            <th scope="col">%</th>
-                        </tr>
-                    </thead>
-    
-                    <tbody>
-    
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Testeddddddddddddddddddddddddddddddddddddddddddddd</td>
-                            <td>100</td>
-                            <td>10</td>
-                            <td>5</td>
-                            <td>50%</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+        <div class="col-auto">
+            <br>
+            <table class="table table-bordered table-striped table-hover table-light table-responsive" id="ranking">
+
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">NOME</th>
+                        <th scope="col">P</th>
+                        <th scope="col">J</th>
+                        <th scope="col">V</th>
+                        <th scope="col">%</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Primeiro lugar</td>
+                        <td>100</td>
+                        <td>10</td>
+                        <td>10</td>
+                        <td>100%</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Segundo lugar</td>
+                        <td>50</td>
+                        <td>10</td>
+                        <td>5</td>
+                        <td>50%</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Terceiro lugar</td>
+                        <td>0</td>
+                        <td>10</td>
+                        <td>0</td>
+                        <td>0%</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
+    </div>
 
 </div>
 
